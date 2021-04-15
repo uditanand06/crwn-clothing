6d1228ac8c265a9fcc724492f59d3655700c6fef
uditanand06 <uditanand84@gmail.com>
2021-04-15 19:47:47 +0530

    check

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
-import { signInWithGoogle } from '../../firebase/firbase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firbase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -16,10 +16,22 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email,password } = this.state;
+
+        
+        try{ 
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+              email:'',
+              password:''
+            });
+        }catch(error){
+            console.error(error);
+
+        }
   };
 
   handleChange = event => {
